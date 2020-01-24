ad35d3c205dac5c74b5bc99eb3a825f84692051b
amittel <arnulf.mittelstaedt@gmail.com>
2020-01-24 13:59:28 +0100

    del angle show

--- Leg/leg.py
+++ Leg/leg.py
@@ -83,9 +83,9 @@ class Leg:
         #Robots has to do it.
         print("Sending Cmds...")
         servo.JointDrive.doActionAllServo()
-        print("Current angle A: ", self.servoAlpha.getCurrentJointAngle())
-        print("Current angle B: ", self.servoBeta.getCurrentJointAngle())
-        print("Current angle G: ", self.servoGamma.getCurrentJointAngle())
+        #print("Current angle A: ", self.servoAlpha.getCurrentJointAngle())
+        #print("Current angle B: ", self.servoBeta.getCurrentJointAngle())
+        #print("Current angle G: ", self.servoGamma.getCurrentJointAngle())
         time.sleep(3)
         print("--- End Init ---")
         # Used to define which leg and if it's coordinates need to be rotated
