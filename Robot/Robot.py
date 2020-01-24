686fcb20dd710c7e576f259f8da9dc2501f8b896
amittel <arnulf.mittelstaedt@gmail.com>
2020-01-24 10:13:56 +0100

    matplotlib out

--- Robot/Robot.py
+++ Robot/Robot.py
@@ -7,7 +7,7 @@ import copy
 import os
 import math
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 import mpl_toolkits.mplot3d.art3d as art3d
 import enum
